281449f013061d8d5840b991ad81b28c07986f19
Alex Valerio <62916245+AlexV713@users.noreply.github.com>
2022-03-15 16:42:58 -0700

    Added New CSS Files & Mobile Header
    I am replacing the old CSS file to a SCSS file for better styling. I added the mobile header and the navigation links and icons. The mobile nav works as a dropdown menu when clicked or tapped.

--- portfolio-app/src/components/Header.js
+++ portfolio-app/src/components/Header.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaGithub, FaLinkedin } from "react-icons/fa"
+import { FaGithub, FaLinkedin, FaBars } from "react-icons/fa"
 
-function header() {
+function Header() {
+    const [visible, setVisible] = React.useState(false);
   return (
     <header>
-        <Link to="/" id="title">
-            <h1>Alejandro Valerio</h1>
-        </Link>
+        <div id="desktop-nav">
+            <Link to="/" id="title">
+                <h1>Alejandro Valerio</h1>
+            </Link>
 
-        <nav id="desktop-nav">
-            <Link to="/" className="links">Home</Link>
-            <Link to="work" className="links">My Work</Link>
-            <Link to="about" className="links">About Me</Link>
+            <nav id="desktop-nav">
+                <Link to="/" className="links">Home</Link>
+                <Link to="work" className="links">My Work</Link>
+                <Link to="about" className="links">About Me</Link>
+                    <div id="icons-container">
+                        <a href="https://github.com/AlexV713" className="icon-links" target="_blank" rel="noreferrer">
+                            <FaGithub/>&nbsp;
+                        </a>
+                        <a href="https://www.linkedin.com/in/alejandro-valerio-9b57b31b0/" className="icon-links" target="_blank" rel="noreferrer">
+                            <FaLinkedin/>
+                        </a>
+                    </div>
+            </nav>
+        </div>
+
+        <div id="mobile-header">
+            <div id="front">
+                <Link to="/" id="mobile-title">
+                    <h1>AV</h1>
+                </Link>
+                <FaBars className="toggle-bars" onClick={() => setVisible(!visible)}/>
+            </div>
+            <nav id="mobile-navigation" style={{display: visible ? 'block' : 'none'}}>
+                <ul id="links-list">
+                    <li><Link to="/" className="links">Home</Link></li>
+                    <li><Link to="work" className="links">My Work</Link></li>
+                    <li><Link to="about" className="links">About Me</Link></li>
+                </ul>
+            </nav>
+            <div id="mobile-icons" style={{display: visible ? 'block' : 'none'}}>
                 <div id="icons-container">
                     <a href="https://github.com/AlexV713" className="icon-links" target="_blank" rel="noreferrer">
                         <FaGithub/>&nbsp;
@@ -21,9 +49,10 @@ function header() {
                         <FaLinkedin/>
                     </a>
                 </div>
-        </nav>
+            </div>
+        </div>
     </header>
   )
 }
 
-export default header
+export default Header
